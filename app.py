0f7b954c14607bf023c2eca05aa3505da6609d38
NIHARIKA TYAGI <niharika.tyagi710@gmail.com>
2025-09-27 14:16:03 +0530

    add title

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ from db_functions import(
 )
 
 st.sidebar.title("Inventory Management Dashboard")
+option = st.sidebar.radio("Select Option :",["Basic Information","Operational Tasks"])
+
+st.title('Inventory and Supply chain Dashboard')
+db = connect_to_db()
+cursor = db.cursor()
+
+if option == "Basic Information":
+    st.header("Basic Matrics")
+
+    basic_info = get_basic_info(cursor)
